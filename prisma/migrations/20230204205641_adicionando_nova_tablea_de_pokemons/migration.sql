65970070e2d1ee0f70f4f5962d2b34745bd50661
Bruno_Barros <barrosbrunofelipe@gmail.com>
2023-02-05 21:47:22 -0300

    feat: preparando ambiente para deploy - teste1

--- prisma/migrations/20230204205641_adicionando_nova_tablea_de_pokemons/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `species` to the `TB_POKEMONS` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "TB_POKEMONS" ADD COLUMN     "species" TEXT NOT NULL;
